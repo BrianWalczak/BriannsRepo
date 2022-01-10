9731d471f8f1191dd83a9730bd4770cd9d8f0403
BrianWalczak <brian@brianwalczak.com>
2022-01-10 16:37:37 -0600

    Added resources to create tweaks

---
+++ Hashes.sh
@@ -0,0 +1,3 @@
+md5 /Users/brianwalczak/Documents/GitHub/BriannsRepo/debs/FILE.deb
+shasum -a 1 /Users/brianwalczak/Documents/GitHub/BriannsRepo/debs/FILE.deb
+shasum -a 256 /Users/brianwalczak/Documents/GitHub/BriannsRepo/debs/FILE.deb
